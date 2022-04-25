07b31c20e39d77f94114305bac46ba62c8031ea8
Kyle <kyle.d.connour+gitkraken@gmail.com>
2022-04-24 21:32:47 -0600

    Add function to compute Rayleigh CO2 optical depth

--- pyrt/rayleigh.py
+++ pyrt/rayleigh.py
@@ -1,5 +1,6 @@
 import numpy as np
-from pyrt.spectral import wavenumber
+from numpy.typing import ArrayLike
+from pyrt.spectral import wavenumber as wavenum
 
 
 def rayleigh_legendre(n_layers: int, n_wavelengths: int) -> np.ndarray:
@@ -62,3 +63,100 @@ def rayleigh_legendre(n_layers: int, n_wavelengths: int) -> np.ndarray:
     except ValueError as ve:
         message = 'The inputs cannot be cast to ints.'
         raise ValueError(message) from ve
+
+
+def rayleigh_co2_optical_depth(
+        column_density: np.ndarray, wavelength: ArrayLike) -> np.ndarray:
+    r"""Compute the Rayleigh CO :sub:`2` scattering optical depth in each layer
+    given its column density at a range of input wavelengths.
+
+    Parameters
+    ----------
+    column_density: np.ndarray
+        1-dimensional array of the column density in each layer.
+    wavelength: np.ndarray
+        1-dimensional array of the wavelengths [microns] to compute the optical
+        depth at.
+
+    Returns
+    -------
+    np.ndarray
+        The Rayleigh scattering optical depth for CO :sub:`2`.
+
+    Raises
+    ------
+    TypeError:
+        Raised if the column density is not a np.ndarray or if the wavelengths
+        cannot be cast to an ndarray.
+    ValueError:
+        Raised if the wavelengths contain any values outside the range
+        [0.1, 50] (I assume this is the valid range to do radiative transfer).
+
+    Notes
+    -----
+    This algorithm computes the optical depth in each layer i by using
+
+    .. math:
+       \tau_i = N_i * \sigma_i.
+
+    The molecular cross-section is given by laboratory measurements from
+    `Sneep and Ubachs, 2005 <https://doi.org/10.1016/j.jqsrt.2004.07.025>`_.
+
+    Examples
+    --------
+    Compute the Rayleigh scattering optical depth from (very simplified)
+    column densities and wavelengths.
+
+    >>> import numpy as np
+    >>> import pyrt
+    >>> column_density = np.linspace(10**26, 10**27, num=15)
+    >>> wavs = np.linspace(0.2, 1, num=5)
+    >>> rayleigh_od = pyrt.rayleigh_co2_optical_depth(column_density, wavs)
+    >>> rayleigh_od.shape
+    (15, 5)
+
+    Simply sum over the layers to get the column integrated optical depth due
+    to Rayleigh scattering.
+
+    >>> np.sum(rayleigh_od, axis=0)
+    array([0.78456184, 0.03590366, 0.00672394, 0.00208873, 0.00084833])
+
+    """
+
+    def _molecular_cross_section(wavenumber: np.ndarray):
+        number_density = 25.47 * 10 ** 18  # laboratory molecules / cm**3
+        king_factor = 1.1364 + 25.3 * 10 ** -12 * wavenumber ** 2
+        index_of_refraction = _co2_index_of_refraction(wavenumber)
+        return _co2_cross_section(
+            number_density, wavenumber, king_factor, index_of_refraction) \
+               * 10 ** -4
+
+    def _co2_index_of_refraction(wavenumber: np.ndarray) -> np.ndarray:
+        n = 1 + 1.1427 * 10 ** 3 * (
+                5799.25 / (128908.9 ** 2 - wavenumber ** 2) +
+                120.05 / (89223.8 ** 2 - wavenumber ** 2) +
+                5.3334 / (75037.5 ** 2 - wavenumber ** 2) +
+                4.3244 / (67837.7 ** 2 - wavenumber ** 2) +
+                0.00001218145 / (2418.136 ** 2 - wavenumber ** 2))
+        return n
+
+    def _co2_cross_section(
+            number_density: float,
+            wavenumber: np.ndarray,
+            king_factor: np.ndarray,
+            index_of_refraction: np.ndarray) \
+            -> np.ndarray:
+        coefficient = 24 * np.pi ** 3 * wavenumber ** 4 / number_density ** 2
+        middle_term = ((index_of_refraction ** 2 - 1) /
+                       (index_of_refraction ** 2 + 2)) ** 2
+        return coefficient * middle_term * king_factor  # cm**2 / molecule
+
+    wavenumber = wavenum(wavelength)
+    try:
+        colden = column_density[:, None]
+    except TypeError as te:
+        message = 'The column density must be a np.ndarray.'
+        raise TypeError(message) from te
+    mol_cs = _molecular_cross_section(wavenumber)[:, None]
+    scattering_od = np.multiply(colden[:, None, :], mol_cs[None, :])
+    return np.squeeze(scattering_od)
--- pyrt/tests/test_rayleigh.py
+++ pyrt/tests/test_rayleigh.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from pyrt import rayleigh_legendre
+from pyrt import rayleigh_legendre, rayleigh_co2_optical_depth
 
 
 class TestRayleighLegendre:
@@ -19,3 +19,30 @@ class TestRayleighLegendre:
     def test_moment_2_is_always_05(self):
         rleg = rayleigh_legendre(100, 100)
         assert np.all(rleg[2] == 0.5)
+
+
+class TestRayleighCO2OpticalDepth:
+    @pytest.fixture
+    def wavelengths(self):
+        yield np.linspace(0.2, 40, num=50)
+
+    @pytest.fixture
+    def column_density(self):
+        yield np.linspace(10**25, 10**26, num=10)
+
+    def test_optical_depth_monotonically_decreases(self, column_density, wavelengths):
+        od = rayleigh_co2_optical_depth(column_density, wavelengths)
+        column_od = np.sum(od, axis=0)
+        assert np.all(np.diff(column_od) < 0)
+
+    def test_int_colden_raises_type_error(self, wavelengths):
+        with pytest.raises(TypeError):
+            rayleigh_co2_optical_depth(10**25, wavelengths)
+
+    def test_too_small_wavelength_raises_value_error(self, column_density):
+        with pytest.raises(ValueError):
+            rayleigh_co2_optical_depth(column_density, 0.05)
+
+    def test_too_large_wavelength_raises_value_error(self, column_density):
+        with pytest.raises(ValueError):
+            rayleigh_co2_optical_depth(column_density, 51)

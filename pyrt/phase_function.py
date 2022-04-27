9d01f8defe8cb409901093d3f805d8677488ed88
Kyle <kyle.d.connour+gitkraken@gmail.com>
2022-04-27 11:15:03 -0600

    Add function to compute HG phase function

--- pyrt/phase_function.py
+++ pyrt/phase_function.py
@@ -223,3 +223,76 @@ def decompose(phase_function: ArrayLike,
     except np.linalg.LinAlgError as lae:
         message = 'The inputs did not make a positive definite matrix.'
         raise ValueError(message) from lae
+
+
+def construct_hg(asymmetry_parameter: ArrayLike,
+                 scattering_angles: ArrayLike) \
+        -> np.ndarray:
+    r"""Construct Henyey-Greenstein phase functions from asymmetry parameters.
+
+    Parameters
+    ----------
+    asymmetry_parameter: ArrayLike
+        N-dimensional array of asymmetry paramters. All values must be between
+        -1 and 1.
+    scattering_angles: ArrayLike
+        1-dimensional array of scattering angles [degrees].
+
+    Returns
+    -------
+    np.ndarray
+        N-dimensional arrray of phase functions with a shape of
+        ``scattering_angles.shape + asymmetry_parameter.shape``.
+
+    Notes
+    -----
+    The Henyey-Greenstein phase function (per solid angle) is defined as
+
+    .. math::
+
+       p(\theta) = \frac{1}{4\pi} \frac{1 - g^2}
+                    {[1 + g^2 - 2g \cos(\theta)]^\frac{3}{2}}
+
+    where :math:`p` is the phase function, :math:`\theta` is the scattering
+    angle, and :math:`g` is the asymemtry parameter.
+
+    .. warning::
+       The normalization for the Henyey-Greenstein phase function is not the
+       same as for a regular phase function. For this phase function,
+
+       .. math::
+          \int_{4\pi} p(\theta) = 1
+
+       *not* 4 :math:`\pi`! To normalize it simply multiply the output by
+       4 :math:`\pi`.
+
+    Examples
+    --------
+    Construct phase functions having 181 scattering angles from an array of
+    asymmetry parameters.
+
+    >>> import numpy as np
+    >>> import pyrt
+    >>> sa = np.linspace(0, 180, num=181)
+    >>> g = 0.5
+    >>> hg_pf = pyrt.construct_hg(g, sa)
+    >>> hg_pf.shape
+    (181,)
+
+    This function also works with an array of asymmetry parameters.
+
+    >>> g = [-1, -0.5, 0, 0.5, 1]
+    >>> pyrt.construct_hg(g, sa).shape
+    (181, 5)
+
+    """
+    g = _AsymmetryParameter(asymmetry_parameter)
+    sa = _ScatteringAngles(scattering_angles)
+    try:
+        cos_sa = np.cos(np.radians(sa))
+        denominator = (1 + g ** 2 - 2 * np.multiply.outer(cos_sa, g)) ** (
+                    3 / 2)
+        return 1 / (4 * np.pi) * (1 - g ** 2) / denominator
+    except ValueError as ve:
+        message = 'The arrays must have the same shapes.'
+        raise ValueError(message) from ve
--- pyrt/tests/test_phase_function.py
+++ pyrt/tests/test_phase_function.py
@@ -4,10 +4,6 @@ from pyrt import decompose, construct_hg
 
 
 class TestDecompose:
-    @pytest.fixture
-    def angles(self) -> np.ndarray:
-        yield np.array([0, 10, 70])
-
     def test_negative_phase_function_raises_value_error(self):
         pf = [-1, 1]
         sa = [10, 20]
@@ -25,3 +21,30 @@ class TestDecompose:
         sa = np.linspace(0, 100, num=50)
         with pytest.raises(ValueError):
             decompose(pf, sa, 51)
+
+
+class TestConstructHG:
+    @pytest.fixture
+    def g(self) -> np.ndarray:
+        yield np.array([[0.5, -0.6], [1, 0.2]])
+
+    @pytest.fixture
+    def sa(self) -> np.ndarray:
+        yield np.linspace(0, 180, num=181)
+
+    def test_function_gives_known_values(self, sa):
+        g = 0
+        pf = construct_hg(g, sa)
+        assert np.all(pf == 1 / (4 * np.pi))
+
+    def test_g_greater_than_1_raises_value_error(self, sa):
+        with pytest.raises(ValueError):
+            construct_hg(np.nextafter(1, 2), sa)
+
+    def test_g_less_than_negative_1_raises_value_error(self, sa):
+        with pytest.raises(ValueError):
+            construct_hg(np.nextafter(-1, -2), sa)
+
+    def test_shape_is_scattering_angles_then_g(self, g, sa):
+        pf = construct_hg(g, sa)
+        assert pf.shape == sa.shape + g.shape
